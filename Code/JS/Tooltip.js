852fd0d32c61c01cae9c0f91e447f15810caab80
Hannes Harnisch <hannes.harnisch@gmx.net>
2021-02-04 17:14:42 +0100

    Updating robots and sitemap

--- Code/JS/Tooltip.js
+++ Code/JS/Tooltip.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
 	$(".tooltip-E").replaceWith("<a data-toggle='tooltip' title='Einzelausstellung'>(E)</a>");
-    $(".tooltip-G").replaceWith("<a data-toggle='tooltip' title='Gruppenausstellung'>(G)</a>");
-    $(".tooltip-K").replaceWith("<a data-toggle='tooltip' title='Katalog'>(K)</a>");
-    $("[data-toggle='tooltip']").tooltip(); 
+	$(".tooltip-G").replaceWith("<a data-toggle='tooltip' title='Gruppenausstellung'>(G)</a>");
+	$(".tooltip-K").replaceWith("<a data-toggle='tooltip' title='Katalog'>(K)</a>");
+	$("[data-toggle='tooltip']").tooltip(); 
 });
